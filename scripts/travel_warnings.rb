3c5abb68a569d76b63300b5e58ab0691d42e5271
John Gadbois <jgadbois@gmail.com>
2016-02-23 14:29:29 -0500

    Add travel alerts to script in addition to travel warnings

--- scripts/travel_warnings.rb
+++ scripts/travel_warnings.rb
@@ -14,3 +14,18 @@ warnings.each do |w|
 
   puts "#{data[:title]} for #{data[:date]}"
 end
+
+@doc = Nokogiri::HTML(open("http://travel.state.gov/_res/rss/TAs.xml/"))
+warnings = @doc.xpath("//item")
+
+warnings.each do |w|
+  data =  { 
+    title: w.xpath("title")[0].content,
+    date: w.xpath("pubdate")[0].content,
+    link: w.xpath("link")[0].content,
+    country: w.xpath("identifier")[0].content,
+    description: w.xpath("description")[0].content,
+  }
+
+  puts "#{data[:title]} for #{data[:date]}"
+end
