554926e6dedcf63f77f7399caf4dcf77a9443169
John Gadbois <jgadbois@gmail.com>
2016-03-20 16:45:36 -0400

    change /login to a POST

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -80,7 +80,7 @@ public class Main {
         before("/testauth", new RequiresAuthenticationFilter(config, "HeaderClient"));
         
         //accept basic auth info in HTTPS header, return token
-        get("/login", (req, res) -> {        	
+        post("/login", (req, res) -> {        	
         	final UserProfile profile = getUserProfile(req, res);
         	System.out.println(profile);
     		JwtGenerator generator = new JwtGenerator(JWT_SALT);
