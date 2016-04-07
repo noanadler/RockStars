0605d6ffb661b614f7bf86be5f442dc5c2d1f737
John Gadbois <jgadbois@gmail.com>
2016-04-06 22:50:52 -0400

    Delete user after logout

--- Frontend/safe-travels/app/controllers/application.js
+++ Frontend/safe-travels/app/controllers/application.js
@@ -1,7 +1,9 @@
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  //user: Ember.inject.service('user'),
   actions: {
   invalidateSession() {
+    this.get('session').set('data.user', null );
     this.get('session').invalidate();
   }
 }
