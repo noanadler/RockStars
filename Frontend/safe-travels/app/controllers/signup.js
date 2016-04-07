c858afc2873cd7fd4c4e0a0c5abde1c55d0452bf
John Gadbois <jgadbois@gmail.com>
2016-04-06 21:34:18 -0400

    Added name to signup for FHIR

--- Frontend/safe-travels/app/controllers/signup.js
+++ Frontend/safe-travels/app/controllers/signup.js
@@ -6,29 +6,33 @@ export default Ember.Controller.extend({
 
   actions: {
     signup: function() {
-      var credentials = this.getProperties('email', 'password'),
+      var credentials = this.getProperties('email', 'password', 'name'),
         authenticator = 'authenticator:jwt',
         user = this.get('user'),
         session = this.get('session'),
         controller = this;
 
-      Ember.$.post(ENV.APP.apiUrl + '/signup', JSON.stringify(credentials)).then(function() {
-        session.authenticate(authenticator, { identification: credentials.email, password: credentials.password }).then(function() {
-          //authenticated
-          session.authorize('authorizer:token', (header, token) => {
-            var headers = {}
-            headers[header] = token;
+      if(!credentials.email || !credentials.password || !credentials.name) {
+        this.set('error', true);
+      } else {
+        Ember.$.post(ENV.APP.apiUrl + '/signup', JSON.stringify(credentials)).then(function() {
+          session.authenticate(authenticator, { identification: credentials.email, password: credentials.password }).then(function() {
+            //authenticated
+            session.authorize('authorizer:token', (header, token) => {
+              var headers = {}
+              headers[header] = token;
 
-            Ember.$.ajax({
-              url: ENV.APP.apiUrl + '/currentuuid',
-              headers: headers
-            }).then(function(data) {
-              session.set('data.user', data );
-              controller.transitionToRoute('setup');
+              Ember.$.ajax({
+                url: ENV.APP.apiUrl + '/currentuuid',
+                headers: headers
+              }).then(function(data) {
+                session.set('data.user', data );
+                controller.transitionToRoute('setup');
+              });
             });
           });
         });
-      });
+      }
     }
   }
 });
