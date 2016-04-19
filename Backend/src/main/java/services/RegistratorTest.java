83bc7539dfc5bd2eb3ea4ee5effed196d0c3c1f7
noanadler <nnadler3@gatech.edu>
2016-04-18 19:09:08 -0700

    Added tests to RegistratorTest

--- Backend/src/main/java/services/RegistratorTest.java
+++ Backend/src/main/java/services/RegistratorTest.java
@@ -2,10 +2,12 @@ package services;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
-
-import javax.print.attribute.standard.PrinterLocation;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
@@ -15,9 +17,6 @@ import org.sql2o.Sql2o;
 import org.sql2o.quirks.PostgresQuirks;
 import org.sql2o.quirks.Quirks;
 
-import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
-
-import ca.uhn.fhir.model.dstu2.resource.TestScript.Setup;
 import data.ArrayConverter;
 import data.HerokuDataSource;
 import models.Sql2oModel;
@@ -30,7 +29,8 @@ public class RegistratorTest {
 	private String userPassword;
 	private String userEmail;
 	private String userGender = "F";
-	private String[] countries = {"Brazil"};	
+	private String country = "Brazil";
+	private String[] countries = {country};	
 	private Boolean verified = true;
 	private Boolean notification = true;
 	private UUID uId;
@@ -63,42 +63,58 @@ public class RegistratorTest {
 	}
 	
 	@Test
-	public void testBeginRegistration() {
+	public void testBeginRegistration1() {
+		long millis=System.currentTimeMillis();
+		String datetime=new Date().toString();
+        datetime=datetime.replace(" ", "");
+        datetime=datetime.replace(":", "");
+        datetime=datetime.replace("GMT", "");
+        String rndchars=RandomStringUtils.randomAlphanumeric(16);
+        String str = rndchars + datetime + millis;
+        userName = "test_name_" + str;
+        userPassword = "test_password_" + str;
+        userEmail = "test_email_" + str + "@gmail.com";
+		StringBuilder errorReporter = new StringBuilder();
+		UUID uuid = registrator.beginRegistration(userEmail, userPassword, userName, userGender, errorReporter);
+		assertNotNull(uuid);
 
 	}
 	
-	/* 
-	 * The method uIdExists in the Registrator class 
-	 * returns true if the user does not exist and false if the user exists.
-	 * Shoudln't it be the opposite? 
+	/*
+	 * When user exist beginRegistration should return null
 	 */
 	@Test
-	public void testUidExists1() {
-        assertEquals(true, registrator.uIdExists(uId));
+	public void testBeginRegistration2() {
+		StringBuilder errorReporter = new StringBuilder();
+		UUID uuid = registrator.beginRegistration(userEmail, userPassword, userName, userGender, errorReporter);
+		assertEquals(null, uuid);
 	}
 	
-	/*
-	 * This test fails since the method models.Sql2oModel.getUserByUid 
-	 * cannot deal with a uId that does not exists
-	 */
+
+	@Test
+	public void testUidExists1() {
+        assertTrue(registrator.uIdExists(uId));
+	}
+	
+
 	@Test
 	public void testUidExists2() {
 		UUID falseUID = UUID.randomUUID();
-		assertEquals(false, registrator.uIdExists(falseUID));
+		assertFalse(registrator.uIdExists(falseUID));
 	}
 	
 	@Test
 	public void testIsSubscribedToNotifications1(){
 		user.setNotifications(true);
 		_model.updateUser(user);
-		assertEquals(true, registrator.isSubscribedToNotifications(uId));
+		assertTrue(registrator.isSubscribedToNotifications(uId));
     }
 	
 	@Test
 	public void testIsSubscribedToNotifications2(){
 		user.setNotifications(false);
 		_model.updateUser(user);
-		assertEquals(false, registrator.isSubscribedToNotifications(uId));
+		assertFalse(registrator.isSubscribedToNotifications(uId));
     }
 	
 	@Test
@@ -107,12 +123,12 @@ public class RegistratorTest {
     	_model.updateUser(user);
     	registrator.verifyUser(uId);
     	user =  _model.getUserByUid(uId);
-    	assertEquals(true, user.getVerified());
+    	assertTrue(user.getVerified());
     }
 	
 	@Test
     public void testEmailExists1(){
-		assertEquals(true, registrator.emailExists(userEmail));
+		assertTrue(registrator.emailExists(userEmail));
     }
 	
 	@Test
@@ -125,7 +141,7 @@ public class RegistratorTest {
         String rndchars=RandomStringUtils.randomAlphanumeric(16);
         String str = rndchars + datetime + millis;
         userEmail = "test_email_" + str + "@gmail.com";
-		assertEquals(false, registrator.emailExists(userEmail));
+		assertFalse(registrator.emailExists(userEmail));
     }
 	
 	/*
@@ -142,15 +158,22 @@ public class RegistratorTest {
 		assertEquals(verified, user.getVerified());
 		assertEquals(notification, user.getNotifications());
     }
-	
+		
+	 //This test fails due to issue #2 - ql2oModel.getCountrySubscribers throws the following exception:
+	 //Database error: ERROR: relation "country_lists" does not exist
 	@Test
     public void testUpdateNotificationCountries(){
-    	
-    }
-	
+		String newCountry = "Mexico";
+    	String[] selectedCountries = {newCountry};
+    	registrator.updateNotificationCountries(userEmail, selectedCountries);
+    	assertEquals(newCountry, user.getCountries());	
+    }	
+
+	 
 	@Test
     public void testGetCountrySubscribers(){
-    	
+		List<String> countrySub = _model.getCountrySubscribers(country);
+		assertEquals(countrySub, registrator.getCountrySubscribers(country));
     }
 	
 	@Test
@@ -159,21 +182,56 @@ public class RegistratorTest {
 		_model.updateUser(user);
 		registrator.setNotificationsStatus(uId, true);
 		user =  _model.getUserByUid(uId);
-		assertEquals(true, user.getNotifications());
+		assertTrue(user.getNotifications());
     }
 	
+	 //This test fails due to issue #2 - ql2oModel.getCountrySubscribers throws the following exception:
+	 //Database error: ERROR: relation "country_lists" does not exist
 	@Test
 	public void testDeregisterFromNotifications(){
-		
+		Boolean isSubscribed = false;
+		String[] countries = user.getCountries();
+		registrator.deregisterFromNotifications(uId, false);
+		for(String c : countries) {
+			List<String> subs = _model.getCountrySubscribers(c);
+			for(String s : subs) {
+				if(s.equals(userEmail)) {
+					isSubscribed = true;
+				}
+			}
+		}
+		assertTrue(isSubscribed);
+
     }
 	@Test
 	public void testGetRegisteredAt(){
-  
+		Date registred = user.getRegistered_at();
+		assertEquals(registred, registrator.getRegisteredAt(uId));
     }
 	
+	/*
+	 * Testing registration that is not expired
+	 */
 	@Test
-    public void testIsRegistrationExpired(){
-    	
+    public void testIsRegistrationExpired1(){
+		Date today = new Date();
+    	user.setRegistered_at(today);
+    	_model.updateUser(user);
+    	assertEquals(false, registrator.isRegistrationExpired(uId));
+    }
+	
+	/*
+	 * Testing registration that is expired
+	 * This method fails due to issue #3
+	 */
+	@Test
+    public void testIsRegistrationExpired2(){
+		LocalDate date = LocalDate.of( 2011 , Month.FEBRUARY , 11 );
+		Date moreThenYearAgo = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    	user.setRegistered_at(moreThenYearAgo);
+    	_model.updateUser(user);
+    	assertEquals(true, registrator.isRegistrationExpired(user.getId()));
+
     }
 	
 	@Test
@@ -190,3 +248,4 @@ public class RegistratorTest {
     	assertEquals(false, registrator.isUserVerified(uId));
     }
 }
+
